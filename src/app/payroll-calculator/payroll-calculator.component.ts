e296780b3e2c302f50b91464fa5d428591021c38
Gabriel <gabriel@gabriels-mbp-2.lan>
2021-12-17 13:36:10 +0100

    fix

--- src/app/payroll-calculator/payroll-calculator.component.ts
+++ src/app/payroll-calculator/payroll-calculator.component.ts
@@ -1,47 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { PayrollDataService } from './service/payroll-data-service.component';
 
+export class PayRollFormModel {
+  public experience: number;
+  public occupation: string
+  public city: string
+  public incomeYear: string;
+}
+
 @Component({
   selector: 'app-payroll-calculator',
   templateUrl: './payroll-calculator.component.html',
-  styleUrls: ['./payroll-calculator.component.scss']
-  ,providers:  [ PayrollDataService ]
+  styleUrls: ['./payroll-calculator.component.scss'],
+  providers: [PayrollDataService]
 })
 export class PayrollCalculatorComponent implements OnInit {
-  occupations: {} = {};
-  basicLocationTaxRates: {} = {};
-  // occupations = { 'Programmer': 1000, 'Teacher': 1000, 'Cashier': 1000 }
-  incomeYears: {} = {};
-  // incomeYears = { '2020': 0.70, '2019': 0.71 };
-  // occupations: string[] = ['Programmer','Teacher','Cashier'];
-  cities: string[] = ['Stockholm', 'Gothenburg'];
   data: any
-  occupation: any;
-  incomeYear: any;
-  city: any;
-  // incomeYears: number[] = [2020,2019];
-
+  payRollFormModel: PayRollFormModel;
+  incomeYearOptions: number[];
 
   constructor(private payrollDataService: PayrollDataService) {
-
-    // console.log(payrollDataService.data)
-    // this.data = payrollDataService.data;
-  }
-
-  click(form:any){
-console.log(form)
   }
 
   ngOnInit(): void {
-    this.data = require('../../data/data.json');
+    this.data = this.payrollDataService.getData();
 
-    setTimeout(() => {
-      console.log(this.occupation)
-    }, 5000);
-    // this.data = data;
-    // console.log(this.payrollDataService.getData())
-    // this.basicLocationTaxRates = require('../../data/data.json').basicLocationTaxRate;
-    // console.log(this.basicLocationTaxRates)
-    // this.incomeYears = require('../../data/data.json');
+    const years = this.data.basicLocationTaxRate
+      .map((tax: any) => tax.incomeYear)
+
+    this.incomeYearOptions = [... new Set(years)] as number[];
+
+
+
+    this.payRollFormModel = new PayRollFormModel();
+  }
+
+  onSubmit(value: PayRollFormModel) {
+    console.log(value)
   }
 }
--- src/app/payroll-calculator/service/payroll-data-service.component.ts
+++ src/app/payroll-calculator/service/payroll-data-service.component.ts
@@ -3,16 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class PayrollDataService{
-  data: {} = {}
-  ngOnInit() {
-    // this.data = require("../../../data/data.json");
-    this.data = require('../../../data/data.json');
-    console.log(this.data);
-  }
+export class PayrollDataService {
 
-  getData(){
-    // console.log(this.data)
-    // return this.data;
+  getData() {
+    const data = require('../../../data/data.json');
+    return data;
   }
 }
